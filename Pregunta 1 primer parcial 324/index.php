b6734ae5ef7862c15b41b3a22a191c740e6f9c4f
CarlosAntonioPoloSanchez <105763178+CarlosAntonioPoloSanchez@users.noreply.github.com>
2023-04-16 18:09:28 -0400

    Add files via upload

---
+++ Pregunta 1 primer parcial 324/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>FCPN</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+  <h1>Carlos Antonio Polo Sanchez</h1>
+  <h1>Facultad de ciencias puras y naturales</h1>
+  <a href="./mate/index.php">Matematica.</a>
+  <br></br>
+  <a href="./informatica/index.php">Informatica.</a>
+  <br></br>
+  <a href="./fisica/index.php">Fisica.</a>
+</body>
+</html>
